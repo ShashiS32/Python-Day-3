8ae40a2b39e3d22f4e5a9d137a922abbf839b3ad
Shashwat Singh <46977833+ShashiS32@users.noreply.github.com>
2024-04-01 18:15:35 +0000

    else if elif

---
+++ main.py
@@ -0,0 +1,8 @@
+height = int(input("What is your height in inches? "))
+
+if height == 100:
+  print("You are the perfect height")
+elif height >= 60:
+  print("You are tall enough")
+elif height < 60:
+  print ("Sorry your not tall enough")
