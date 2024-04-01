5814f18349cd1edd590671fdf9c408733ba854fb
Shashwat Singh <46977833+ShashiS32@users.noreply.github.com>
2024-04-01 18:36:19 +0000

    Love calc

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-age = int(input("How old are you? "))
-if age <= 8:
-  print("Sorry you are too young to ride this ride")
-else:
-  height = int(input("How tall are you? "))
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+name1 = name1.lower()
+name2 = name2.lower()
+t = name1.count("t")
+r = name1.count("r")
+u = name1.count("u")
+e = name1.count("e")
+l = name1.count("l")
+o = name1.count("o")
+v = name1.count("v")
+
+first_digit = t + r + u + e + l + o + v
+
+t = name2.count("t")
+r = name2.count("r")
+u = name2.count("u")
+e = name2.count("e")
+l = name2.count("l")
+o = name2.count("o")
+v = name2.count("v")
+
+second_digit = t + r + u + e + l + o + v
+
+score = str(first_digit) + str(second_digit)
+
+print(f"Your love score is {score} % !")
 
 
-  if height <= 40 and age > 8:
-    print("You are tall and old enough for this ride")
-  else:
-    print ("Sorry you are too short for this")
+
