36a8bd04f2ddd473b781f2395346d625ffcafdec
Shashwat Singh <46977833+ShashiS32@users.noreply.github.com>
2024-04-01 18:21:14 +0000

    nested if else

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-number = int(input("Enter a number: "))
+age = int(input("How old are you? "))
+height = int(input("How tall are you? "))
 
-if number % 2 == 0:
-  print("This is an even number")
+if age <= 8:
+  print("Sorry you are too young to ride this ride")
 else:
-  print("This is an odd number")
+  if height <= 40:
+    print("Sorry you are too short to ride this ride")
+  else:
+    print("You are tall and old enough for this ride")
