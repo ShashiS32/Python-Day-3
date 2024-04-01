824ab75ef665d13f509dfdf654294ab9484ae826
Shashwat Singh <46977833+ShashiS32@users.noreply.github.com>
2024-04-01 18:26:15 +0000

    made it cleaner

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 age = int(input("How old are you? "))
-height = int(input("How tall are you? "))
-
 if age <= 8:
   print("Sorry you are too young to ride this ride")
 else:
-  if height <= 40:
-    print("Sorry you are too short to ride this ride")
-  else:
+  height = int(input("How tall are you? "))
+
+
+  if height <= 40 and age > 8:
     print("You are tall and old enough for this ride")
+  else:
+    print ("Sorry you are too short for this")
